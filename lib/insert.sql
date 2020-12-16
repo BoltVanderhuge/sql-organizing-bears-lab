02c8052a02ab07ccc35c3d590e0164707bfe5570
john_wisneski <john.j.wisneski@gmail.com>
2020-12-16 16:47:25 -0500

    commit tree 5ead69d2e31dd2766cba8c63bc6bc50a34adccaa

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "M", "brown", "bearly hanging on", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2 , "M", "black", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1 , "F", "spotted", "witchy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5 , "M", "brown", "ordered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4 , "F", "white", "normy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7 , "M", "hot pink", "grinch-like", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2 , "F", "blue", "hungry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6 , "M", "brown", "jealous", 1);
